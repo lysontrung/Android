de1902f299985ea46f95a454d9725f0899dc2db3
lysontrung <lysontrung95@gmail.com>
2016-10-08 11:48:59 +0700

    First commit

---
+++ Test_git_android/app/src/androidTest/java/lysontrung/test_git_android/ApplicationTest.java
@@ -0,0 +1,13 @@
+package lysontrung.test_git_android;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ Test_git_android/app/src/main/java/lysontrung/test_git_android/MainActivity.java
@@ -0,0 +1,13 @@
+package lysontrung.test_git_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
